85d1392b0ef4eea97cd71a5245b068df6ce04d18
Stefan Wiebe <stefanwiebe96@gmail.com>
2022-02-13 21:12:21 +0100

    feat: adjust button styles

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -6,8 +6,10 @@
 	{type}
 	on:click
 	class="
-    py-4 px-7 rounded-md 
+    py-3 px-5 rounded-lg 
     bg-slate-800 text-slate-50 shadow shadow-slate-800/30
     hover:bg-slate-900 hover:shadow-slate-900/30
+    focus-visible:outline-none
+    focus-visible:ring focus-visible:ring-slate-800 focus-visible:ring-opacity-50
     transition ease-out"><slot /></button
 >
